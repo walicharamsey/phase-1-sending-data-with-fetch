95cd30517964c24d603a0675f06bc0f8935b6e32
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-10-18 08:28:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+      name: userName,
+      email: userEmail
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML = `New user ID: ${data.id}`;
+      })
+      .catch(error => {
+        document.body.innerHTML = `Error: ${error.message}`;
+      });
+  }
+  
